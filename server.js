f9e32d194b12298b1d1046af6ca904589e6c4cec
OpatileKelobang <49315650+OpatileKelobang@users.noreply.github.com>
2020-11-30 14:25:51 +0200

    Form processing

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -7,3 +9,28 @@ app.get('/*', function (req, res) {
         '/dist/opatile-kelobang/index.html'));
 });
 app.listen(process.env.PORT || 8080);
+
+const nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
+
+let mailOptions = {
+    from: process.env.EMAIL,
+    to: process.env.EMAIL,
+    subject: '',
+    text: ''
+};
+
+transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+        console.log('Unable to send');
+    } else {
+        console.log('Email sent');
+    }
+});
+
